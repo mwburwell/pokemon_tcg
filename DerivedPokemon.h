190aa8c3ce349ba0adee749c4e4ec266e2b3be3f
mwburwell <mwburwell@gmail.com>
2021-04-30 14:10:17 -0400

    Added a Pokemon Template

--- DerivedPokemon.h
+++ DerivedPokemon.h
@@ -203,3 +203,37 @@ public:
 	std::string getAttack3Name() {return this->attack3Name;}
 
 };
+
+
+/**
+ * Pokemon Template
+ * */
+/*
+class __________ : public Pokemon {		// <-- Name your pokemon
+private:
+	string attack1Name;
+	string attack2Name;
+	string attack3Name;
+	Element* element;
+public:
+	/// <param name = "None"> No parameters </param>
+	_____________() : 									// <-- Add the name of your pokemon to the constructor
+	Pokemon("_________", __) {					// input name of pokemon as first parameter(string), total HP in second parameter (int)
+		this->attack1Name = "___________";		// <-- input the name of first attack; string type (string )
+		this->attack2Name = "___________";		// <-- input the name of second attack; string type (string )
+		this->attack3Name = "___________";		// <-- input the name of third attack or name it critical second attack; (string )
+		this->element = new ___________();		// <--- input your pokemon's element TYPE
+												// elements to choose from: Psychic, Colorless, Fire, Grass, Water, Lightning 
+	}
+	~Geodude() {
+		delete element;
+		element = NULL;
+	}
+
+	Element* getElement() { return this->element;}
+	std::string getAttack1Name() {return this->attack1Name;}
+	std::string getAttack2Name() {return this->attack2Name;}
+	std::string getAttack3Name() {return this->attack3Name;}
+
+};
+*/
